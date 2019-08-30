405e02ce96af7683b2e20578e2a19d1fd961cb10
Garrett Thompson <gtcruz.gt@gmail.com>
2019-08-30 02:56:23 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,7 +1,7 @@
 require 'nokogiri'
 require 'open-uri'
 
-html = open("https://flatironschool.com/")
+html = open("http://flatironschool.com/")
+doc = Nokogiri::HTML(html)
 
-
-
+puts doc.css(".headline-260IBN")
