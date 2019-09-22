5dfca4e2aa3f2072005463c521e3aa8f4f51a2ad
Christopher Dent <christopherdent01@gmail.com>
2019-09-22 22:21:00 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,13 @@
 require 'nokogiri'
 require 'open-uri'
 
-html = open("https://flatironschool.com/")
+html = open("http://www.flatironschool.com")
+
+doc = Nokogiri::HTML(html)
+
+courses= doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
+
+courses.each do |course|
+  
+  puts course.text.strip
+end 
