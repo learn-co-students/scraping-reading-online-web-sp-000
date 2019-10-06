4a591a2d3be13b2e09cd3068bcb9b1d6d5c7034c
Panela Leung <leungpanela@gmail.com>
2019-10-06 01:14:42 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,7 +1,16 @@
+# require 'nokogiri'
+# require 'open-uri'
+#
+# html = open("https://flatironschool.com/")
+#
+# doc = Nokogiri::HTML(open("https://flatironschool.com/"))
+# doc.css(".headline-26OIBN").text
+# doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
+require "pry"
 require 'nokogiri'
 require 'open-uri'
 
 html = open("https://flatironschool.com/")
-
-doc = Nokogiri::HTML(open("https://flatironschool.com/"))
-doc.css(".headline-26OIBN").text
+doc = Nokogiri::HTML(html)
+binding.pry
+doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
