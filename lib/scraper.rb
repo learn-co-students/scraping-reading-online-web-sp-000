52a26e6c056c144c2d256b48325e8b7d8cd6d8d1
Bruce Ruffin <bruffin1221@gmail.com>
2019-12-16 20:36:20 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,3 +2,7 @@ require 'nokogiri'
 require 'open-uri'
 
 html = open("https://flatironschool.com/")
+Nokogiri::HTML(html)
+doc = Nokogiri::HTML(html)
+doc.css(".headline-26OIBN").text
+html = open('http://www.google.com')
