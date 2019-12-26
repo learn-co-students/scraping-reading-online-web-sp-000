7eb8b3c83a094f86688bbd5e1f9cf957735f4e89
Sergio Marelli <sergio.mrll@gmail.com>
2019-12-26 18:22:45 +0000

    tested

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,3 +2,9 @@ require 'nokogiri'
 require 'open-uri'
 
 html = open("https://flatironschool.com/")
+html = Nokogiri::HTML(html)
+
+p html.css('.buttonBlue-3yHkWz.heading-3RIoNe')[0].text
+puts html.css('.buttonBlue-3yHkWz.heading-3RIoNe')[0].attributes
+puts html.css('.buttonBlue-3yHkWz.heading-3RIoNe')[0].children
+# puts html.css('.buttonBlue-3yHkWz.heading-3RIoNe')[0].each {|e| puts e}
