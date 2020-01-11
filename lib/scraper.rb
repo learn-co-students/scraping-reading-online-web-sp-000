371ad54afc534b21b81d637ace05cbcadd0d8765
Nicole Rothenay <nthorn1211@gmail.com>
2020-01-11 23:57:02 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -11,3 +11,8 @@ courses = doc.css("#2a778efd-1685-5ec6-9e5a-0843d6a88b7b .inlineMobileLeft-2Yo00
 courses.each do |course|
   puts course.text.strip
 end
+
+# Scraping resources
+# http://ruby.bastardsbook.com/chapters/html-parsing/
+# https://www.youtube.com/watch?v=oXwdOdBUyCI
+# https://nokogiri.org/tutorials/installing_nokogiri.html
