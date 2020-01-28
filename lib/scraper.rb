89b058660b5bbc4598b4a8b4e4a516d140541dd7
LIManuel <leonmalisov@gmail.com>
2020-01-27 21:04:10 -0500

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,18 @@
+require "pry"
 require 'nokogiri'
 require 'open-uri'
 
+# open-uri
 html = open("https://flatironschool.com/")
+
+# nokogiri
+# use the Nokogiri::HTML method to take the string of HTML 
+# returned by open-uri's open method and convert it into a NodeSet 
+# (aka, a bunch of nested "nodes") that we can easily play around with.
+doc = Nokogiri::HTML(html)
+
+courses = doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC").css(".text-4GLMvr").css(".title-oE5vT4") #can also use .strip with .text
+
+courses.each do |course|
+  puts course.text.strip
+end
