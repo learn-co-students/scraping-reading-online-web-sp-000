44c460d13d03bcb123e7bda47c5695ea7a04c812
Mark A Guza <guzamaguza@gmail.com>
2020-02-12 21:39:42 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,7 @@
 require 'nokogiri'
 require 'open-uri'
 
-html = open("https://flatironschool.com/")
+html = open("https://flatironschool.com/")  #open the file
+doc = Nokogiri::HTML(html) #save the HTML data in a bunch of nodes
+doc.css(".headline-26OIBN").text  #call the css selector(class for this specific html piece) on doc
+        #.text allows us to just get the text out of the html 
