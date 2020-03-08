cd98e365a2b963e690b3c31fae1cb1bc925cce77
Austin Rhoads <austinrrhoads@ymail.com>
2020-03-08 20:27:44 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,7 @@
 require 'nokogiri'
 require 'open-uri'
 
-html = open("https://flatironschool.com/")
+html = open("https://norse-mythology.org/tales/norse-creation-myth/")
+doc = Nokogiri::HTML(html)
+titles = doc.css('strong').text
+puts doc.css('.entry-title').text
