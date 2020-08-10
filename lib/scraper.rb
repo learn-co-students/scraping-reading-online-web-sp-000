6e4fec90ddc55dccfde29b73814dbc209a656145
Caitlin Hinshaw <caitlin.hinshaw@gmail.com>
2020-08-10 03:49:04 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,3 +8,9 @@ doc = Nokogiri::HTML(html)
 doc.css(".headline-26OIBN").text.strip
 
 puts doc.css(".headline-26OIBN").text.strip
+
+courses = doc.css("#id-3c239880-48cf-5ce7-84d0-578546d87586 .inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")
+
+courses.each do |course|
+  puts course.text.strip
+end
