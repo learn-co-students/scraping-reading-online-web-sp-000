d9992cab60caebed4fca2d93d255992185a5180e
Alexander Clamor <alexander.clamor@gmail.com>
2020-08-28 23:16:56 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,3 +2,13 @@ require 'nokogiri'
 require 'open-uri'
 
 html = open("https://flatironschool.com/")
+
+doc = Nokogiri::HTML(html)
+
+# puts doc.css(".headline-26OIBN").text.strip
+
+courses = doc.css(".inlineMobileLeft-2Yo002")
+ 
+courses.each do |course|
+  puts course.text.strip
+end
