d26a618c9de0231131ac2c79c56bfb8a45f95dbc
fut0k0 <cccabell@icloud.com>
2020-11-17 16:25:15 -0800

    Completed lab

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,11 @@
-require 'nokogiri'
-require 'open-uri'
+require "nokogiri"
+require "open-uri"
 
-html = open("https://flatironschool.com/")
+doc = Nokogiri::HTML(URI.open("https://flatironschool.com/"))
+puts doc.css(".headline-26OIBN")
+puts doc.css(".headline-26OIBN").text.strip
+
+courses = doc.css("#id-3c239880-48cf-5ce7-84d0-578546d87586 .inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")
+courses.each{|course| puts course.text.strip}
+puts courses[0].name
+puts courses[0].attributes
