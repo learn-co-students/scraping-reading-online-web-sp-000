46e0c1e225e3f6b629bab48c9313a984b42a3b51
Samuel DeSantis <samuel.i.desantis@gmail.com>
2020-12-23 17:18:58 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,8 @@
 require 'nokogiri'
 require 'open-uri'
 
-html = open("https://flatironschool.com/")
+doc = Nokogiri::HTML(open("https://flatironschool.com/")) #=> Grabs html from site
+                            #=> Converts to NodeSet
+                            #=> Nested Nodes refers to any tree of elemnts in which parent
+                            #=> elements branch off ot contain children elements
+puts doc.css('inner=3PLBeK inner-3LPBeK')
