b00613b3120f92199ac74371bfcc7324251c88e2
Manuel Eduardo Baez Diaz <baezdiazm@yahoo.com>
2021-01-05 20:04:29 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,3 +2,12 @@ require 'nokogiri'
 require 'open-uri'
 
 html = open("https://flatironschool.com/")
+doc = Nokogiri::HTML(html)
+
+courses = doc.css("#id-3c239880-48cf-5ce7-84d0-578546d87586 .title-oE5vT4")
+puts "Current amount of elements in #courses: #{courses.size}
+
+"
+ courses.each do |item|
+   puts "* #{item.text.strip}"
+ end
