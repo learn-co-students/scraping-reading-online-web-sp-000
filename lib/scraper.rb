ecb4de8ee4400a94ae82db24eb497b598baa2343
Kevin Ortiz <kevin.ortiz@flatironschool.com>
2021-01-20 02:17:29 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,3 +2,5 @@ require 'nokogiri'
 require 'open-uri'
 
 html = open("https://flatironschool.com/")
+doc = Nokogiri::HTML(open("https://twitter.com/home"))
+puts doc.css(css-1dbjc4n .r-18u37iz).text
