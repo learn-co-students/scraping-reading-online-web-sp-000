1514cbd2d3f7d9ab145bcce2d42621054bd14edd
A.J. Romaniello <ajiellodev@gmail.com>
2021-01-26 17:59:37 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,3 +2,14 @@ require 'nokogiri'
 require 'open-uri'
 
 html = open("https://flatironschool.com/")
+
+doc = Nokogiri::HTML(html)
+courses = doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")
+p courses[0].children[1].children[0].text
+p courses[0].children[1].children[2].text
+p courses[1].children[1].children[0].text
+p courses[1].children[1].children[2].text
+p courses[2].children[1].children[0].text
+p courses[2].children[1].children[2].text
+p courses[3].children[1].children[0].text
+p courses[3].children[1].children[2].text
