5be86a4e810ef3157b0751b8c71d20856765fc75
A.J. Romaniello <ajiellodev@gmail.com>
2021-01-26 18:04:14 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,11 +5,7 @@ html = open("https://flatironschool.com/")
 
 doc = Nokogiri::HTML(html)
 courses = doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")
-p courses[0].children[1].children[0].text
-p courses[0].children[1].children[2].text
-p courses[1].children[1].children[0].text
-p courses[1].children[1].children[2].text
-p courses[2].children[1].children[0].text
-p courses[2].children[1].children[2].text
-p courses[3].children[1].children[0].text
-p courses[3].children[1].children[2].text
+courses.each { |course|
+  p "Course: #{course.children[1].children[0].text}"
+  p "Description: #{course.children[1].children[2].children[0].children[0].text}"
+}
