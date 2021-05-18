76b729f2ec6109b3ba749054183015b67ac4790e
Erika Curry-Elrod <ecurryelrod@gmail.com>
2021-05-18 15:26:14 -0600

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -11,7 +11,9 @@ doc = Nokogiri::HTML(open("https://flatironschool.com/"))
 
 courses = doc.css(".gridContainer-EnvccM .title-oE5vT4")
 
-courses.each do |course|
-    puts course.text.strip
-end
+courses.each {|course| puts course.text.strip}
 
+p doc.css(".gridContainer-EnvccM .title-oE5vT4")[0]
+p doc.css(".gridContainer-EnvccM .title-oE5vT4")[0].name
+p doc.css(".gridContainer-EnvccM .title-oE5vT4")[0].attributes
+p doc.css(".gridContainer-EnvccM .title-oE5vT4")[0].children
